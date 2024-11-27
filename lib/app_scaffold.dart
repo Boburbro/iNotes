074cbc9abc6fb3a21247cbdebc2843036c48e72a
therasuldev <rasul.ramixanov@gmail.com>
2024-11-27 17:55:56 +0400

    new feature [implement user authentication flow with email and password]

--- lib/app_scaffold.dart
+++ lib/app_scaffold.dart
@@ -1,117 +1,297 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:note_app/core/models/note/note.dart';
-import 'package:note_app/core/provider/note_bloc/note_bloc.dart';
-import 'package:note_app/core/provider/note_bloc/note_event.dart';
-import 'package:note_app/view/pages/favorite.dart';
-import 'package:note_app/view/pages/home.dart';
-import 'package:note_app/view/pages/profile.dart';
-import 'package:note_app/view/pages/search.dart';
-import 'package:note_app/view/utilities/extensions.dart';
-import 'package:uuid/uuid.dart';
+import 'package:inotes/core/provider/note_bloc/note_bloc.dart';
+import 'package:inotes/editor_page.dart';
+import 'package:inotes/view/pages/add_category_page.dart';
+import 'package:inotes/view/utilities/colors.dart';
 
-class AppScaffold extends StatefulWidget {
-  const AppScaffold({super.key});
-
-  @override
-  State<AppScaffold> createState() => _AppScaffoldState();
-}
-
-class _AppScaffoldState extends State<AppScaffold> {
-  int _currentIndex = 0;
-  final _pages = const <StatefulWidget>[HomePage(), FavoritePage(), ProfilePage()];
-  final appBarTitles = {
-    0: 'Home',
-    1: 'Favorite',
-    2: 'Profile',
-  };
-
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-
-  void _showCreateNoteDialog() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Create Note'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              TextField(
-                controller: _titleController,
-                decoration: const InputDecoration(
-                  labelText: 'Title',
-                ),
-              ),
-              TextField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(
-                  labelText: 'Description',
-                ),
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                if (_titleController.text.trim().isEmpty || _descriptionController.text.trim().isEmpty) return;
-
-                final note = Note(
-                  id: const Uuid().v4(),
-                  isfavorite: false,
-                  createdAt: DateTime.now().toFormat(),
-                  title: _titleController.text.trim(),
-                  description: _descriptionController.text.trim(),
-                );
-                context.read<NoteBloc>().add(AddNoteEvent(note: note));
-                _titleController.clear();
-                _descriptionController.clear();
-                Navigator.of(context).pop();
-              },
-              child: const Text('Save'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('Cancel'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+class NotesPage extends StatelessWidget {
+  const NotesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(appBarTitles[_currentIndex] ?? ''),
-        actions: _currentIndex == 0
-            ? [
-                IconButton(
-                  onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => const SearchPage(),
-                      )),
-                  icon: const Icon(Icons.search),
-                )
-              ]
-            : null,
-      ),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (currentIndex) => setState(() => _currentIndex = currentIndex),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorite'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _showCreateNoteDialog,
-        child: const Icon(Icons.add),
+    return ColoredBox(
+      color: Colors.white,
+      child: SafeArea(
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppColors.primaryGrey,
+          body: CustomScrollView(
+            slivers: [
+              // Top Section
+              SliverToBoxAdapter(
+                child: Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Hello, Rasul',
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              Text(
+                                'My Notes',
+                                style: TextStyle(color: Colors.black, fontSize: 30),
+                              ),
+                            ],
+                          ),
+                          CircleAvatar(),
+                        ],
+                      ),
+                      const SizedBox(height: 16),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Search your notes',
+                          hintStyle: const TextStyle(color: Colors.black54),
+                          filled: true,
+                          fillColor: AppColors.primaryOrange,
+                          contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 16.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                            borderSide: BorderSide.none,
+                          ),
+                          suffixIcon: Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: IconButton(
+                              icon: const Icon(Icons.search, color: Colors.black),
+                              onPressed: () {},
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Recent Notes Header
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Recent Notes',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('See all'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Recent Notes List
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 140,
+                  child: BlocBuilder<NoteBloc, NoteState>(
+                    builder: (context, state) {
+                      if (state.event == NoteEvents.fetchRecentNotesStart) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      final recentNotes = state.recentNotes?.data;
+
+                      if (recentNotes == null) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      if (recentNotes.isEmpty) {
+                        return const Center(child: Text('No recent notes'));
+                      }
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: recentNotes.length,
+                        itemBuilder: (context, index) {
+                          final recentNote = recentNotes[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AdvancedRichTextEditor(
+                                    category: recentNote.category,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.all(16.0),
+                              padding: const EdgeInsets.all(16.0),
+                              width: 200,
+                              decoration: BoxDecoration(
+                                color: AppColors.primaryGreen,
+                                borderRadius: BorderRadius.circular(25.0),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(recentNote.title, style: const TextStyle(fontSize: 20)),
+                                  const SizedBox(height: 8),
+                                  Text(recentNote.content),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+
+              // Folder Header
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        'Folder',
+                        style: TextStyle(color: Colors.black, fontSize: 20),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          showModalBottomSheet(
+                            context: context,
+                            isScrollControlled: true,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+                            ),
+                            builder: (context) {
+                              return const AddCategoryBottomSheet();
+                            },
+                          );
+                        },
+                        icon: const Icon(Icons.add_box_outlined),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // Folder List
+              BlocBuilder<NoteBloc, NoteState>(
+                builder: (context, state) {
+                  if (state.event == NoteEvents.fetchCategoriesStart) {
+                    return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+                  }
+
+                  final categories = state.categories?.data;
+
+                  if (categories == null) {
+                    return const SliverToBoxAdapter(child: Center(child: CircularProgressIndicator()));
+                  }
+
+                  if (categories.isEmpty) {
+                    return const SliverToBoxAdapter(child: Center(child: Text('No categories')));
+                  }
+
+                  return SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, index) {
+                        final category = categories[index];
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 20.0),
+                          padding: const EdgeInsets.all(16.0),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: AppColors.primaryGreen,
+                            borderRadius: BorderRadius.circular(25.0),
+                          ),
+                          child: Row(
+                            children: [
+                              CircleAvatar(
+                                radius: 20,
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: NetworkImage(category.avatar),
+                              ),
+                              const SizedBox(width: 16),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('${category.notesCount} Notes', style: const TextStyle(fontSize: 20)),
+                                  const SizedBox(height: 8),
+                                  Text(
+                                    category.name,
+                                    style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      childCount: categories.length,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+
+          // Floating Action Button
+          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () {
+
+          //   },
+          //   backgroundColor: AppColors.primaryOrange,
+          //   child: const Icon(Icons.add),
+          // ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) => Container(
+                  height: 200,
+                  color: AppColors.primaryGrey,
+                  child: Column(
+                    children: [
+                      for (var item in [
+                        {'icon': Icons.star, 'category': 'Achievement'},
+                        {'icon': Icons.space_bar, 'category': 'You Space'},
+                        {'icon': Icons.self_improvement, 'category': 'Self Development'}
+                      ])
+                        ListTile(
+                          leading: Icon(item['icon'] as IconData),
+                          title: Text((item['title'] ?? '') as String),
+                          onTap: () {
+                            Navigator.pop(context);
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => AdvancedRichTextEditor(
+                                  category: item['category'] as String,
+                                ),
+                              ),
+                            );
+                          },
+                        )
+                    ],
+                  ),
+                ),
+              );
+            },
+            backgroundColor: AppColors.primaryOrange,
+            child: const Icon(Icons.add),
+          ),
+
+          // Bottom Navigation Bar
+          bottomNavigationBar: Container(height: 80, color: AppColors.primaryGrey),
+        ),
       ),
     );
   }
