73bbd49fa622f2dde380ff7689bd70963d1c6f9e
therasuldev <rasul.ramixanov@gmail.com>
2024-11-27 17:55:35 +0400

    new feature [add AppColors utility class with primary color constants]

---
+++ lib/view/utilities/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryGrey = Color(0xFFF1F1F3);
+  static const Color primaryYellow = Color(0xFFFFE4B0);
+  static const Color primaryOrange = Color(0xFFFFC963);
+  static const Color primaryGreen = Color(0xFFEDFFDF);
+  static const Color primaryRed = Color(0xFFFFE7E2);
+}
