79b4ebf36986f84b79160a07ebdf83b610657a26
therasuldev <rasul.ramixanov@gmail.com>
2024-11-28 15:01:20 +0400

    add add_category_to_db function and update note fetching to include delta field

--- src/db/note.rs
+++ src/db/note.rs
@@ -2,16 +2,19 @@ use r2d2::PooledConnection;
 use r2d2_sqlite::SqliteConnectionManager;
 use rusqlite::{params, Error};
 
-use crate::models::{Category, NewNote, Note};
+use crate::{
+    models::{Category, NewCategory, NewNote, Note},
+    utils::save_image_to_disk,
+};
 
 pub fn add_note_to_db(
     conn: &PooledConnection<SqliteConnectionManager>,
     note: NewNote,
 ) -> Result<Note, Error> {
     match conn.execute(
-        "INSERT INTO notes (title, content, category, created_at, updated_at) 
-        VALUES (?1, ?2, ?3, datetime('now'), NULL)",
-        params![note.title, note.content, note.category],
+        "INSERT INTO notes (title, content, category, delta, created_at, updated_at) 
+        VALUES (?1, ?2, ?3, ?4, datetime('now'), NULL)",
+        params![note.title, note.content, note.category, note.delta],
     ) {
         Ok(_) => (),
         Err(e) => {
@@ -37,6 +40,7 @@ pub fn add_note_to_db(
         title: note.title,
         content: note.content,
         category: note.category,
+        delta: note.delta,
         created_at: chrono::Utc::now().to_rfc3339(),
         updated_at: None,
     })
@@ -48,7 +52,7 @@ pub fn fetch_notes_from_db(
     offset: u16,
 ) -> Result<Vec<Note>, Error> {
     let sql = "
-    SELECT id, title, content, category, created_at, updated_at 
+    SELECT id, title, content, category, delta, created_at, updated_at 
     FROM notes
     LIMIT ?1 OFFSET ?2";
 
@@ -60,6 +64,7 @@ pub fn fetch_notes_from_db(
             title: row.get("title")?,
             content: row.get("content")?,
             category: row.get("category")?,
+            delta: row.get("delta")?,
             created_at: row.get("created_at")?,
             updated_at: row.get("updated_at")?,
         })
@@ -78,7 +83,7 @@ pub fn fetch_recent_notes_from_db(
     conn: &PooledConnection<SqliteConnectionManager>,
 ) -> Result<Vec<Note>, Error> {
     let sql = "
-    SELECT id, title, content, category, created_at, updated_at 
+    SELECT id, title, content, category, delta, created_at, updated_at 
     FROM notes
     ORDER BY created_at DESC
     LIMIT 3";
@@ -90,6 +95,7 @@ pub fn fetch_recent_notes_from_db(
             id: row.get("id")?,
             title: row.get("title")?,
             content: row.get("content")?,
+            delta: row.get("delta")?,
             category: row.get("category")?,
             created_at: row.get("created_at")?,
             updated_at: row.get("updated_at")?,
@@ -103,7 +109,7 @@ pub fn fetch_recent_notes_from_db(
 pub fn fetch_categories_from_db(
     conn: &PooledConnection<SqliteConnectionManager>,
 ) -> Result<Vec<Category>, Error> {
-    let sql = "SELECT id, notes_count, avatar, name FROM categories";
+    let sql = "SELECT id, notes_count, avatar, color, name FROM categories";
     let mut stmt = conn.prepare(sql).unwrap();
 
     let category_iter = stmt.query_map([], |row| {
@@ -112,6 +118,7 @@ pub fn fetch_categories_from_db(
             name: row.get("name")?,
             avatar: row.get("avatar")?,
             notes_count: row.get("notes_count")?,
+            color: row.get("color")?,
         })
     })?;
 
@@ -122,4 +129,31 @@ pub fn fetch_categories_from_db(
     }
 
     Ok(categories)
+}
+
+pub fn add_category_to_db(
+    conn: &PooledConnection<SqliteConnectionManager>,
+    category: NewCategory,
+) -> Result<Category, Error> {
+    let sql = "INSERT INTO categories (name, notes_count, color) VALUES (?1, 0, ?2)";
+
+    conn.execute(sql, params![category.name, category.color])?;
+
+    let result = save_image_to_disk(&category.avatar).ok();
+    let avatar = result.unwrap();
+
+    let id = conn.last_insert_rowid() as u64;
+
+    conn.execute(
+        "UPDATE categories SET avatar = ?1 WHERE id = ?2",
+        params![avatar, id],
+    )?;
+
+    Ok(Category {
+        id: id,
+        name: category.name,
+        avatar: avatar,
+        notes_count: 0,
+        color: category.color,
+    })
 }
