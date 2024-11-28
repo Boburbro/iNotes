213a7551e82baa909a555f29bdd1a795702f8655
therasuldev <rasul.ramixanov@gmail.com>
2024-11-28 12:33:44 +0400

    add color field to Category model and create NewCategory struct with color serialization

--- src/models/category.rs
+++ src/models/category.rs
@@ -6,4 +6,20 @@ pub struct Category {
     pub name: String,
     pub avatar: String,
     pub notes_count: u64,
+    #[serde(serialize_with = "color_to_hex")]
+    pub color: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NewCategory {
+    pub name: String,
+    pub avatar: Vec<u8>,
+    pub color: u32,
+}
+
+fn color_to_hex<S>(color: &u32, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: serde::Serializer,
+{
+    serializer.serialize_str(&format!("0x{:08X}", color))
 }
