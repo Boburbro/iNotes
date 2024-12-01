7fde5c35eb43d31deeb14d5d8f802f4ea8b7babc
therasuldev <rasul.ramixanov@gmail.com>
2024-12-01 17:10:05 +0400

    new feature [add color property and copyWith method to Category model]

--- lib/core/models/category.dart
+++ lib/core/models/category.dart
@@ -3,12 +3,14 @@ class Category {
   final String name;
   final String avatar;
   final int notesCount;
+  final int color;
 
   Category({
     required this.id,
     required this.name,
     required this.avatar,
     required this.notesCount,
+    required this.color,
   });
 
   factory Category.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class Category {
       name: json['name'],
       avatar: json['avatar'],
       notesCount: json['notes_count'],
+      color: int.parse(json['color']),
     );
   }
 
@@ -26,6 +29,23 @@ class Category {
       'name': name,
       'avatar': avatar,
       'notes_count': notesCount,
+      'color': color.toString(),
     };
+  }
+
+  Category copyWith({
+    int? id,
+    String? name,
+    String? avatar,
+    int? notesCount,
+    int? color,
+  }) {
+    return Category(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      avatar: avatar ?? this.avatar,
+      notesCount: notesCount ?? this.notesCount,
+      color: color ?? this.color,
+    );
   }
 }
