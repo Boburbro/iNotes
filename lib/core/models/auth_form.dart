362a391f4576984ac53979f3e29445d6649a5ce9
therasuldev <rasul.ramixanov@gmail.com>
2024-12-01 17:09:46 +0400

    new feature [add LoginForm and RegisterForm models for user authentication]

---
+++ lib/core/models/auth_form.dart
@@ -0,0 +1,21 @@
+class LoginForm {
+  String username;
+  String password;
+
+  LoginForm({
+    required this.username,
+    required this.password,
+  });
+}
+
+class RegisterForm {
+  String email;
+  String username;
+  String password;
+
+  RegisterForm({
+    required this.email,
+    required this.username,
+    required this.password,
+  });
+}
