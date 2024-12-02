ca95fcbc365ec74b034947474b4af69a0bf7e316
therasuldev <rasul.ramixanov@gmail.com>
2024-12-02 22:55:53 +0400

    new feature [enhance NoteService with user and category management for notes, including fetch, add, update, and delete functionalities]

--- lib/core/service/remote/note.dart
+++ lib/core/service/remote/note.dart
@@ -17,6 +17,8 @@ class NoteService {
 
   Future<Note?> addNote({required Map<String, dynamic> noteJson}) async {
     final formData = FormData.fromMap({
+      'user_id': noteJson['user_id'],
+      'category_id': noteJson['category_id'],
       'title': noteJson['title'],
       'content': noteJson['content'],
       'category': noteJson['category'],
@@ -39,9 +41,10 @@ class NoteService {
     }
   }
 
-  Future<PaginatedDataResponse<Note>?> fetchNotes() async {
+  Future<PaginatedDataResponse<Note>?> fetchNotes({required int userId}) async {
+    final queryParameters = {'user_id': userId};
     try {
-      final response = await _dio.get('/notes');
+      final response = await _dio.get('/notes', queryParameters: queryParameters);
       if (response.statusCode == 200) {
         final notes = PaginatedDataResponse<Note>.fromJson(response.data, Note.fromJson);
         return notes;
@@ -55,9 +58,27 @@ class NoteService {
     }
   }
 
-  Future<PaginatedDataResponse<Note>?> fetchRecentNotes() async {
+  Future<PaginatedDataResponse<Note>?> fetchNotesByCategory({required int userId, required int categoryId}) async {
+    final queryParameters = {'user_id': userId, 'category_id': categoryId};
     try {
-      final response = await _dio.get('/recent-notes');
+      final response = await _dio.get('/notes-by-category', queryParameters: queryParameters);
+      if (response.statusCode == 200) {
+        final notes = PaginatedDataResponse<Note>.fromJson(response.data, Note.fromJson);
+        return notes;
+      }
+      throw 'Failed to load notes';
+    } on DioException catch (exception) {
+      throw DioErrorHelper.handle(exception);
+    } catch (e) {
+      CSLog.instance.debug('Fetch Notes Error Message: $e');
+      throw 'Failed to load notes';
+    }
+  }
+
+  Future<PaginatedDataResponse<Note>?> fetchRecentNotes({required int userId}) async {
+    final queryParameters = {'user_id': userId};
+    try {
+      final response = await _dio.get('/recent-notes', queryParameters: queryParameters);
       if (response.statusCode == 200) {
         final recentNotes = PaginatedDataResponse<Note>.fromJson(response.data, Note.fromJson);
         return recentNotes;
@@ -71,9 +92,55 @@ class NoteService {
     }
   }
 
-  Future<PaginatedDataResponse<Category>?> fetchCategories() async {
+  Future<bool> deleteNote({
+    required int userId,
+    required int categoryId,
+    required int noteId,
+  }) async {
+    final queryParameters = {'user_id': userId, 'category_id': categoryId, 'note_id': noteId};
     try {
-      final response = await _dio.get('/categories');
+      final response = await _dio.delete('/note', queryParameters: queryParameters);
+      return response.statusCode == 204;
+    } on DioException catch (exception) {
+      throw DioErrorHelper.handle(exception);
+    } catch (e) {
+      CSLog.instance.debug('Fetch Recent Notes Error Message: $e');
+      throw 'Failed to load recent notes';
+    }
+  }
+
+  Future<Note?> updateNote({
+    required int userId,
+    required int noteId,
+    required String title,
+    required String content,
+    required String delta,
+  }) async {
+    final data = {
+      "user_id": userId,
+      "note_id": noteId,
+      "title": title,
+      "content": content,
+      "delta": delta,
+    };
+    try {
+      final response = await _dio.put('/note', data: data);
+      if (response.statusCode == 200) {
+        return Note.fromJson(response.data);
+      }
+      throw 'Failed to update note';
+    } on DioException catch (exception) {
+      throw DioErrorHelper.handle(exception);
+    } catch (e) {
+      CSLog.instance.debug('Update Note Error Message: $e');
+      throw 'Failed to update note';
+    }
+  }
+
+  Future<PaginatedDataResponse<Category>?> fetchCategories({required int userId}) async {
+    final queryParameters = {'user_id': userId};
+    try {
+      final response = await _dio.get('/categories', queryParameters: queryParameters);
       if (response.statusCode == 200) {
         final categories = PaginatedDataResponse<Category>.fromJson(response.data, Category.fromJson);
         return categories;
@@ -90,6 +157,7 @@ class NoteService {
   Future<Category?> addCategory({required Json categoryJson}) async {
     try {
       final formData = FormData.fromMap({
+        'user_id': categoryJson['user_id'],
         'name': categoryJson['name'],
         'color': colorToHex(categoryJson['color']),
         'avatar': MultipartFile.fromBytes(
