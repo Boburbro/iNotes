70bd2da98da3dff43dc00d8038d9e8a398277536
therasuldev <rasul.ramixanov@gmail.com>
2024-12-04 13:27:13 +0400

    feat: add ProfileHeader and SettingsView for user profile management and theme customization

---
+++ lib/view/pages/settings/settings_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:inotes/core/config/theme.dart';
+import 'package:inotes/core/provider/theme/theme_cubit.dart';
+import 'package:inotes/core/provider/theme/theme_state.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({super.key});
+
+  @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Change theme'),
+              BlocBuilder<ThemeCubit, ThemeState>(
+                builder: (context, state) {
+                  return Switch.adaptive(
+                    value: state.theme == AppTheme.black,
+                    onChanged: (_) {
+                      context.read<ThemeCubit>().changeTheme(state.theme == AppTheme.black ? 'light' : 'black');
+                    },
+                  );
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
