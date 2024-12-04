042c8b36eccc5b02d161df69294500d522a99e6a
therasuldev <rasul.ramixanov@gmail.com>
2024-12-04 12:07:14 +0400

    refactor NoteService: rename file and update color conversion to use ViewUtils

--- lib/core/service/remote/note_service.dart
+++ lib/core/service/remote/note_service.dart
@@ -1,12 +1,10 @@
 import 'package:dio/dio.dart';
-
-import 'package:inotes/core/models/category.dart';
 import 'package:inotes/core/models/note.dart';
 import 'package:inotes/core/models/response.dart';
 import 'package:inotes/core/service/api_client.dart';
 import 'package:inotes/core/service/dio_helper.dart';
 import 'package:inotes/core/service/log_service.dart';
-import 'package:inotes/core/types.dart';
+import 'package:inotes/view/utilities/utils.dart';
 
 class NoteService {
   static NoteService? _instance;
@@ -23,7 +21,7 @@ class NoteService {
       'content': noteJson['content'],
       'category': noteJson['category'],
       'delta': noteJson['delta'],
-      'color': colorToHex(noteJson['color']),
+      'color': ViewUtils.colorToHex(noteJson['color']),
     });
 
     try {
@@ -135,52 +133,6 @@ class NoteService {
       CSLog.instance.debug('Update Note Error Message: $e');
       throw 'Failed to update note';
     }
-  }
-
-  Future<PaginatedDataResponse<Category>?> fetchCategories({required int userId}) async {
-    final queryParameters = {'user_id': userId};
-    try {
-      final response = await _dio.get('/categories', queryParameters: queryParameters);
-      if (response.statusCode == 200) {
-        final categories = PaginatedDataResponse<Category>.fromJson(response.data, Category.fromJson);
-        return categories;
-      }
-      throw 'Failed to load categories';
-    } on DioException catch (exception) {
-      throw DioErrorHelper.handle(exception);
-    } catch (e) {
-      CSLog.instance.debug('Fetch Categories Error Message: $e');
-      throw 'Failed to load categories';
-    }
-  }
-
-  Future<Category?> addCategory({required Json categoryJson}) async {
-    try {
-      final formData = FormData.fromMap({
-        'user_id': categoryJson['user_id'],
-        'name': categoryJson['name'],
-        'color': colorToHex(categoryJson['color']),
-        'avatar': MultipartFile.fromBytes(
-          categoryJson['avatar'],
-          filename: 'avatar.png',
-          contentType: DioMediaType('image', 'png'),
-        ),
-      });
-      final response = await _dio.post('/category', data: formData);
-      if (response.statusCode == 201) {
-        return Category.fromJson(response.data);
-      }
-      throw 'Failed to add category';
-    } on DioException catch (exception) {
-      throw DioErrorHelper.handle(exception);
-    } catch (e) {
-      CSLog.instance.debug('Add Category Error Message: $e');
-      throw 'Failed to add category';
-    }
-  }
-
-  String colorToHex(int value) {
-    return '0x${value.toRadixString(16).toUpperCase()}';
   }
 
   Future<PaginatedDataResponse<Note>?> searchforNotes({required String query}) async {
