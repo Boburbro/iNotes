29b1260828085b37f448926ddd18f7474ab730cf
therasuldev <rasul.ramixanov@gmail.com>
2024-12-04 17:10:14 +0400

    feat: add method to AppState for obtaining connection with foreign keys enabled

--- backend/src/state/state.rs
+++ backend/src/state/state.rs
@@ -1,6 +1,17 @@
-use r2d2::Pool;
+use r2d2::{Pool, PooledConnection};
 use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::Error;
 
 pub struct AppState {
     pub pool: Pool<SqliteConnectionManager>,
+}
+
+impl AppState {
+    pub fn get_conn_with_foreign_keys(
+        &self,
+    ) -> Result<PooledConnection<SqliteConnectionManager>, Error> {
+        let conn: r2d2::PooledConnection<SqliteConnectionManager> = self.pool.get().unwrap();
+        conn.execute("PRAGMA foreign_keys = ON;", []).unwrap();
+        Ok(conn)
+    }
 }
