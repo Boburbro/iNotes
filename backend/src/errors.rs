b8558315ade017670854281b90f3a152e81143f4
therasuldev <rasul.ramixanov@gmail.com>
2024-12-08 20:12:46 +0400

    feat: reorganize DatabaseError enum for improved clarity and error handling

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -2,29 +2,30 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DatabaseError {
-    #[error("Rusqlite error")]
-    Rusqlite(#[from] rusqlite::Error),
+    // Authentication errors
+    #[error("Email already exists")]
+    EmailExists,
 
-    #[error("Serde JSON error")]
-    SerdeJson(#[from] serde_json::Error),
+    #[error("Username or password incorrect")]
+    IncorrectCredentials,
+
+    #[error("User not found")]
+    UserNotFound,
+
+    #[error("Username already exists")]
+    UsernameExists,
 
     // Category errors
     #[error("Category already exists")]
     CategoryExists,
 
-    // Authentication errors
-    #[error("Email already exists")]
-    EmailExists,
-
-    #[error("Username already exists")]
-    UsernameExists,
-
-    #[error("Username or password incorrect")]
-    IncorrectCredentials,
-
+    // Other errors
     #[error("Internal server error")]
     InternalServerError,
 
-    #[error("User not found")]
-    UserNotFound,
+    #[error("Rusqlite error")]
+    Rusqlite(#[from] rusqlite::Error),
+
+    #[error("Serde JSON error")]
+    SerdeJson(#[from] serde_json::Error),
 }
